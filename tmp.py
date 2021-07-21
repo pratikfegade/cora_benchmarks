30f08ff598519c7e5409346b953039585504b915
Pratik Fegade <pratikfegade@gmail.com>
2021-07-20 22:58:33 -0400

    Better runtime support

--- tmp.py
+++ tmp.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 import utils
 import run_utils
 import tvm
@@ -5,14 +7,28 @@ from tvm import tir, te
 from tvm.te import RangeDimension as Dim
 from tvm.tir import UninterpFun as Uf
 
-BATCH_SIZE = 2
-MAX_LEN = 128
+parser = argparse.ArgumentParser()
+parser.add_argument('--target', nargs='?', default='llvm')
+parser.add_argument('--dtype', dest='dtype', nargs='?', default='float32')
+parser.add_argument('--max-batches', dest='max_batches', default=1, type=int)
+parser.add_argument('--batch-size', dest='batch_size', default=32, type=int)
+parser.add_argument('--peel-loops', dest='peel_loops', default=False, action='store_true')
+parser.add_argument('--unroll-loops', dest='unroll_loops', default=False, action='store_true')
+parser.add_argument('--debug', dest='debug', default=False, action='store_true')
+parser.add_argument('--debug-code', dest='debug_code', default=False, action='store_true')
+parser.add_argument('--manual-code', dest='manual_code', default=False, action='store_true')
+parser.add_argument('--dense-storage', dest='dense_storage', default=False, action='store_true')
+parser.add_argument('--dataset', nargs='?', default='random')
+parser.add_argument('--datadir', nargs='?', default='random')
+args = parser.parse_args()
+
 NUM_HEADS = 8
 HEAD_SIZE = 64
 TILE=64
 RTILE=4
+MAX_LEN = utils.ceilmult(run_utils.get_dataset_max_len(args.dataset), TILE)
 
-lens = te.placeholder((BATCH_SIZE,), name = 'lens', dtype = 'int32')
+lens = te.placeholder((args.batch_size,), name = 'lens', dtype = 'int32')
 
 bd = Dim('bd')
 md = Dim('md')
@@ -24,7 +40,7 @@ def len_uf(name): return Uf(name, (64, MAX_LEN), [bd], lambda b: utils.ceilmult(
 
 luf = len_uf('s')
 ls =  {
-    0: Uf.from_constant('bd', BATCH_SIZE),
+    0: Uf.from_constant('bd', args.batch_size),
     1: Uf.from_constant('md', NUM_HEADS),
     2: luf,
     3: luf,
@@ -32,37 +48,36 @@ ls =  {
 }
 
 loop_ufs=[ls[0], ls[1], ls[2], ls[4]]
-width_ufs=loop_ufs
-Q = te.ragged_placeholder((BATCH_SIZE, NUM_HEADS, MAX_LEN, HEAD_SIZE), [bd, md, s1, hd], loop_ufs,
+width_ufs = None if args.dense_storage else loop_ufs
+Q = te.ragged_placeholder((args.batch_size, NUM_HEADS, MAX_LEN, HEAD_SIZE), [bd, md, s1, hd], loop_ufs,
                           name='Q', width_ufs=width_ufs)
 
 loop_ufs=[ls[0], ls[1], ls[3], ls[4]]
-width_ufs=loop_ufs
-K = te.ragged_placeholder((BATCH_SIZE, NUM_HEADS, MAX_LEN, HEAD_SIZE), [bd, md, s2, hd], loop_ufs,
+width_ufs = None if args.dense_storage else loop_ufs
+K = te.ragged_placeholder((args.batch_size, NUM_HEADS, MAX_LEN, HEAD_SIZE), [bd, md, s2, hd], loop_ufs,
                           name='K', width_ufs=width_ufs)
 
 loop_ufs=[ls[0], ls[1], ls[2], ls[3]]
-width_ufs=[loop_ufs]
+width_ufs = None if args.dense_storage else [loop_ufs]
 k = tvm.reduce_axis((0, HEAD_SIZE), name = 'k')
-O = te.ragged_compute((BATCH_SIZE, NUM_HEADS, MAX_LEN, MAX_LEN), [bd, md, s1, s2], loop_ufs,
+O = te.ragged_compute((args.batch_size, NUM_HEADS, MAX_LEN, MAX_LEN), [bd, md, s1, s2], loop_ufs,
                         lambda ds: tvm.sum(Q[ds[bd], ds[md], ds[s1], k] * K[ds[bd], ds[md], ds[s2], k], axis = k),
                         name = 'O', width_uf_lists=width_ufs)
 
 s = tvm.create_schedule([O.op])
 
-target = "cuda"
-if target == "cuda":
+if args.target == "cuda":
+    thread_x = lambda: tvm.thread_axis("threadIdx.x")
+    thread_y = lambda: tvm.thread_axis("threadIdx.y")
+    block_x = lambda: tvm.thread_axis("blockIdx.x")
+    block_y = lambda: tvm.thread_axis("blockIdx.y")
+
     thread_x = lambda: tvm.thread_axis("threadIdx.x")
     thread_y = lambda: tvm.thread_axis("threadIdx.y")
     block_x = lambda: tvm.thread_axis("blockIdx.x")
     block_y = lambda: tvm.thread_axis("blockIdx.y")
 
     Ol = s.cache_write(O, "local")
-    # Qs = s.cache_read(Q, "shared", [Ol])
-    # Ks = s.cache_read(K, "shared", [Ol])
-
-    # Ql = s.cache_read(Qs, "local", [Ol])
-    # Kl = s.cache_read(Ks, "local", [Ol])
 
     b, h, x, y = s[O].leaf_iter_vars[0:4]
     xo, xi = s[O].split(x, factor = 64)
@@ -73,34 +88,83 @@ if target == "cuda":
     f2 = s[O].fuse(b, f1)
     f = s[O].fuse(f2, h)
     s[O].bind(f, block_x())
+
+    f = s[O].fuse(xi, yi)
+    fo, fi = s[O].split(f, factor = 256)
+    fio, fii = s[O].split(fi, factor = 16)
+    # xio, xii = s[O].split(xi, factor = 16)
+    # yio, yii = s[O].split(yi, factor = 16)
+    # s[O].reorder()
+    s[O].bind(fio, thread_y())
+    s[O].bind(fii, thread_x())
+    s[Ol].compute_at(s[O], fo)
+
+    # x, y, k = s[Ol].leaf_iter_vars[2], s[Ol].leaf_iter_vars[3], s[Ol].leaf_iter_vars[4]
+    # s[Ol].reorder(k, x, y)
+
+
+
+    # Ol = s.cache_write(O, "local")
+    # Qs = s.cache_read(Q, "shared", [Ol])
+    # Ks = s.cache_read(K, "shared", [Ol])
+
+    # Ql = s.cache_read(Qs, "local", [Ol])
+    # Kl = s.cache_read(Ks, "local", [Ol])
+
+    # b, h, x, y = s[O].leaf_iter_vars[0:4]
+    # xo, xi = s[O].split(x, factor = 64)
+    # yo, yi = s[O].split(y, factor = 64)
+
+    # s[O].reorder(b, xo, yo, h, xi, yi)
+    # f1 = s[O].fuse(xo, yo)
+    # f2 = s[O].fuse(b, f1)
+    # f = s[O].fuse(f2, h)
+    # s[O].bind(f, block_x())
     # s[Qs].compute_at(s[O], f)
     # s[Ks].compute_at(s[O], f)
 
-    xio, xii = s[O].split(xi, nparts = 16)
-    yio, yii = s[O].split(yi, nparts = 16)
-    s[O].reorder(xio, yio, xii, yii)
-    s[O].bind(xio, thread_y())
-    s[O].bind(yio, thread_x())
-    s[Ol].compute_at(s[O], yio)
+    # xio, xii = s[O].split(xi, nparts = 16)
+    # yio, yii = s[O].split(yi, nparts = 16)
+    # s[O].reorder(xio, yio, xii, yii)
+    # s[O].bind(xio, thread_y())
+    # s[O].bind(yio, thread_x())
+    # s[Ol].compute_at(s[O], yio)
 
-    x, y, k = s[Ol].leaf_iter_vars[2], s[Ol].leaf_iter_vars[3], s[Ol].leaf_iter_vars[4]
-    s[Ol].reorder(k, x, y)
+    # x, y, k = s[Ol].leaf_iter_vars[2], s[Ol].leaf_iter_vars[3], s[Ol].leaf_iter_vars[4]
+    # s[Ol].reorder(k, x, y)
+    # if args.unroll_loops:
+    #     s[Ol].unroll(x)
+    #     s[Ol].unroll(y)
     # s[Ql].compute_at(s[Ol], k)
     # s[Kl].compute_at(s[Ol], k)
 
     # x, y = s[Qs].leaf_iter_vars[2], s[Qs].leaf_iter_vars[3]
     # xo, xi = s[Qs].split(x, nparts = 16)
-    # yo, yi = s[Qs].split(y, nparts = 16)
-    # s[Qs].bind(xo, thread_y())
-    # s[Qs].bind(yo, thread_x())
+    # s[Qs].reorder(y, xi, xo)
+    # s[Qs].bind(xo, thread_x())
+    # f = s[Qs].fuse(y, xi)
+    # fo, fi = s[Qs].split(f, factor = 16)
+    # s[Qs].bind(fi, thread_y())
+    # s.reorder_tensor_dimensions(Qs, 2, 3)
+    # s.split_tensor_dimension(Qs, 3, 4)
+    # s.reorder_tensor_dimensions(Qs, 3, 4)
 
     # x, y = s[Ks].leaf_iter_vars[2], s[Ks].leaf_iter_vars[3]
     # xo, xi = s[Ks].split(x, nparts = 16)
-    # yo, yi = s[Ks].split(y, nparts = 16)
-    # s[Ks].bind(xo, thread_y())
-    # s[Ks].bind(yo, thread_x())
+    # s[Ks].reorder(y, xi, xo)
+    # s[Ks].bind(xo, thread_x())
+    # f = s[Ks].fuse(y, xi)
+    # fo, fi = s[Ks].split(f, factor = 16)
+    # s[Ks].bind(fi, thread_y())
+    # s.reorder_tensor_dimensions(Ks, 2, 3)
+    # s.split_tensor_dimension(Ks, 3, 4)
+    # s.reorder_tensor_dimensions(Ks, 3, 4)
 
-    tvm_callback_cuda_compile = tvm.register_func(utils.get_tvm_callback_cuda_compile(256))
+    suffix = ""
+    gen_prefix = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0] + suffix
+    _ = tvm.register_func(utils.get_tvm_callback_cuda_compile(256))
+    _ = tvm.register_func(
+        utils.get_tvm_callback_cuda_postproc(args, os.path.realpath(__file__), fileprefix=gen_prefix))
 else:
     Ol = s.cache_write(O, "local")
     Qs = s.cache_read(Q, "shared", [Ol])
@@ -132,13 +196,17 @@ else:
     s[Kl].compute_at(s[Ol], k)
 
 inputs = [[lens], [Q, K, O]]
-# stmt = tvm.lower(s, inputs, simple_mode = True)
-# print(stmt)
-fadd, i_bufs = tvm.build(s, inputs, target)
-# fadd = tvm.runtime.module.load_module('/home/ppf/rnn_compilers/ragged_tensors/incubator-tvm/build/qkt.so')
-# print('-----GPU code-----\n' + fadd.imported_modules[0].get_source())
-# print(fadd.get_source())
-# print('-----GPU code-----\n' + str(i_bufs))
-l_input = run_utils.random_lengths(BATCH_SIZE, MAX_LEN)
-print(((l_input + 63) // 64) * 64)
-run_utils.run(fadd, [l_input], i_bufs, [Q, K, O], target)
+if args.debug_code:
+    lowered = tvm.lower(s, inputs, simple_mode = True)
+    print(lowered)
+    # fadd = tvm.build(s, inputs, args.target)
+    # if args.target == 'cuda':
+    #     print('-----GPU code-----\n' + fadd.imported_modules[0].get_source())
+    # else:
+    #     print('-----CPU code-----\n' + fadd.get_source())
+else:
+    fadd, i_bufs = tvm.build(s, inputs, args.target)
+    # fadd = tvm.runtime.module.load_module('/home/ppf/rnn_compilers/ragged_tensors/incubator-tvm/build/qkt.so')
+    l_input = run_utils.random_lengths(args.batch_size, MAX_LEN)
+    run_utils.run(fadd, [l_input], i_bufs, [Q, K, O], args.batch_size, args.max_batches,
+                  args.dataset, args.datadir, args.target, args.debug)
