742f0089dec02848f1224c4e39f6ef9f75cd3290
Pratik Fegade <pratikfegade@gmail.com>
2021-09-18 12:21:05 -0400

    WIP on pre_linear ARM

--- bert_layer/tvm/pre_linear_cpu.py
+++ bert_layer/tvm/pre_linear_cpu.py
@@ -73,58 +73,47 @@ O = te.ragged_compute((QKV_NUM, BATCH_SIZE, MAX_LEN, NUM_HEADS, OUT_SIZE), [qkv,
 s = tvm.create_schedule([O.op])
 
 if True:
-    # Ol = s.cache_write(O, 'local')
+    O_local = S
 
-    q, b, l, h, o = s[O].leaf_iter_vars
-    l = s[O].fuse(b, l, padding=128)
+    O_local_q_c, O_local_b_c, O_local_m_c, O_local_h_c, O_local_n_c, O_local_k = (tuple(O_local.op.axis) +
+                                                                                  tuple(O_local.op.reduce_axis))
+    O_local_m_c = s[O_local].fuse(O_local_b_c, O_local_m_c)
+
+    O_local_m_c_o_i, O_local_m_c_i = s[O_local].split(O_local_m_c, factor=4)
+    O_local_m_c_o_o_i, O_local_m_c_o_i = s[O_local].split(O_local_m_c_o_i, factor=16)
+
+    O_local_n_c_o_i, O_local_n_c_i = s[O_local].split(O_local_n_c, factor=4)
+    O_local_k_o, O_local_k_i = s[O_local].split(O_local_k, factor=64)
+
+    s[O_local].reorder(O_local_m_c_o_o_i, O_local_k_o, O_local_m_c_o_i, O_local_n_c_o_i, O_local_k_i, O_local_m_c_i, O_local_n_c_i)
+
+    s[O_local].unroll(O_local_n_c_i)
+    s[O_local].unroll(O_local_m_c_i)
+
+    q, b, x, h, y = s[O].leaf_iter_vars
+    x = s[O].fuse(b, x, padding=64)
+    xo, xi = s[O].split(x, factor = 64)
+    yo, yi = s[O].split(y, factor = 64)
+    s[O].reorder(xo, yo, h, xi, yi)
+    f = s[O].fuse(xo, yo)
+    s[O].parallel(f)
 
 
+    O_m, O_n = xi, yi
+    O_m_o_i, O_m_i = s[O].split(O_m, factor=64)
+    O_m_o_o, O_m_o_i = s[O].split(O_m_o_i, factor=1)
+    O_n_o_i, O_n_i = s[O].split(O_n, factor=16)
+    O_n_o_o, O_n_o_i = s[O].split(O_n_o_i, factor=4)
+    s[O].reorder(O_m_o_o, O_n_o_o, O_m_o_i, O_n_o_i, O_m_i, O_n_i)
+    s[O_local].compute_at(s[O], O_n_o_i)
 
-    # O_local = S
+    s[O_local].vectorize(O_local_n_c_i)
+    s[O].vectorize(O_n_i)
 
-    # As = s.cache_read(A, "shared", [S], loop_layout=[ls[0], ls[2], ls[1], ls[3]], layouts=[ls[0], ls[2], ls[1], ls[3]])
-    # Ws = s.cache_read(W, "shared", [O_local], vanilla=True)
+    s[O_local].set_scope('local')
 
-    # O_local_b_c, O_local_m_c, O_local_n_c, O_local_k = tuple(O_local.op.axis) + tuple(O_local.op.reduce_axis)
-    # O_local_m_c = s[O_local].fuse(O_local_b_c, O_local_m_c, padding=4)
-    # O_local_m_c_o_i, O_local_m_c_i = s[O_local].split(O_local_m_c, factor=4)
-    # O_local_n_c_o_i, O_local_n_c_i = s[O_local].split(O_local_n_c, factor=64)
-    # O_local_k_o, O_local_k_i = s[O_local].split(O_local_k, factor=16)
-    # s[O_local].reorder(O_local_m_c_o_i, O_local_n_c_o_i, O_local_k_o, O_local_k_i, O_local_m_c_i, O_local_n_c_i)
-
-    # b, x, y = s[O].leaf_iter_vars[0:3]
-    # x = s[O].fuse(b, x, padding=64)
-    # xo, xi = s[O].split(x, factor = 64)
-    # yo, yi = s[O].split(y, factor = 64)
-    # s[O].reorder(xo, yo, xi, yi)
-    # f = s[O].fuse(xo, yo)
-    # s[O].parallel(f)
-
-    # O_m, O_n = xi, yi
-    # O_m_o_i, O_m_i = s[O].split(O_m, factor=4)
-
-    # O_n_o_i, O_n_i = s[O].split(O_n, factor=64)
-    # O_n_o_o, O_n_o_i = s[O].split(O_n_o_i, factor=1)
-    # s[O].reorder(O_m_o_i, O_n_o_o, O_n_o_i, O_m_i, O_n_i)
-    # s[O_local].compute_at(s[O], O_n_o_i)
-    # s[As].compute_at(s[O], O_n_o_i)
-    # s[Ws].compute_at(s[O_local], O_local_k_o)
-
-    # s[O_local].vectorize(O_local_n_c_i)
-    # s[O].vectorize(O_n_i)
-
-    # inputs = [[lens], [BS_VAR, A, W, O]]
-
-    # s.fuse_tensor_dimensions(O_local, 0, 1)
-    # s.fuse_tensor_dimensions(As, 0, 1)
-
-    # b, l, h, i = s[As].leaf_iter_vars
-    # s[As].fuse(b, l)
-
-    # s[S].set_scope('local')
-    # s[S].mark_no_bounds_check()
-    # s[As].mark_no_bounds_check()
-    pass
+    s.fuse_tensor_dimensions(O_local, 1, 2)
+    s[S].mark_no_bounds_check()
 else:
     pass
 
@@ -139,9 +128,10 @@ def size_fn(l_inputs):
                                              run_utils.prefix_sum(len(lens), lambda b: out_fn(b)))
     }
 
-bQKV = tvm.decl_buffer([BATCH_SIZE*MAX_LEN, IN_SIZE], name = "bQKV")
-binds = {QKV: bQKV}
-inputs = [[lens], [BS_VAR, bQKV, W, B, O]]
+# bQKV = tvm.decl_buffer([BATCH_SIZE*MAX_LEN, IN_SIZE], name = "bQKV")
+# binds = {QKV: bQKV}
+binds={}
+inputs = [[lens], [BS_VAR, QKV, W, B, O]]
 
 name = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0]
 out, batches = run_utils.lower_or_build(name, s, inputs, args, size_fn=size_fn, binds=binds, pad_sum=64,
@@ -151,11 +141,11 @@ q_size = 0
 for length in batches[0]:
     q_size += utils.ceilmult(length, 64) * NUM_HEADS * OUT_SIZE
 
-ctr = 0
-_, QKV, W, B, O  = out
-O = O.flatten()
-for length in batches[0]:
-    this_extent = length * NUM_HEADS * OUT_SIZE
-    print(length, np.mean(O[ctr:ctr + this_extent]), np.mean(O[ctr+q_size:ctr+q_size + this_extent]),
-          np.mean(O[ctr+2*q_size:ctr+2*q_size + this_extent]))
-    ctr += utils.ceilmult(length, 64) * NUM_HEADS * OUT_SIZE
+# ctr = 0
+# _, QKV, W, B, O  = out
+# O = O.flatten()
+# for length in batches[0]:
+#     this_extent = length * NUM_HEADS * OUT_SIZE
+#     print(length, np.mean(O[ctr:ctr + this_extent]), np.mean(O[ctr+q_size:ctr+q_size + this_extent]),
+#           np.mean(O[ctr+2*q_size:ctr+2*q_size + this_extent]))
+#     ctr += utils.ceilmult(length, 64) * NUM_HEADS * OUT_SIZE
