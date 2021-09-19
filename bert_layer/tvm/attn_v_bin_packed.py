bdc68bef95692edda2d71c07d2578b46433ac267
Pratik Fegade <pratikfegade@gmail.com>
2021-09-19 21:07:44 +0000

    Better schedule for attn_v bin packed

--- bert_layer/tvm/attn_v_bin_packed.py
+++ bert_layer/tvm/attn_v_bin_packed.py
@@ -19,7 +19,7 @@ MAX_LEN = run_utils.get_maxlen_padded(args.dataset)
 assert MAX_LEN > 64
 NUM_HEADS = 8
 HEAD_SIZE = 64
-red_tile = 16
+red_tile = 8
 
 lens = te.placeholder((BATCH_SIZE,), name = 'lens', dtype = 'int32')
 
@@ -79,56 +79,56 @@ nty = 8
 def schedule_op(O, tile, suffix):
     Ol = s.cache_write(O, 'local')
 
-    # As = s.cache_read(A, 'shared', [Ol], suffix=suffix)
-    # Vs = s.cache_read(V, 'shared', [Ol], suffix=suffix)
+    As = s.cache_read(A, 'shared', [Ol], suffix=suffix)
+    Vs = s.cache_read(V, 'shared', [Ol], suffix=suffix)
 
-    # Al = s.cache_read(As, 'local', [Ol], suffix=suffix)
-    # Vl = s.cache_read(Vs, 'local', [Ol], suffix=suffix)
+    Al = s.cache_read(As, 'local', [Ol], suffix=suffix)
+    Vl = s.cache_read(Vs, 'local', [Ol], suffix=suffix)
 
     b, x, h, y = s[O].leaf_iter_vars[0:4]
     xo, xi = s[O].split(x, factor = tile)
 
     s[O].reorder(b, xo, h, xi, y)
     f = s[O].fuse(b, xo)
-    s[O].bind(f, block_x())
-    s[O].bind(h, block_y())
+    s[O].bind(f, block_y())
+    s[O].bind(h, block_x())
 
     xio, xii = s[O].split(xi, factor = nty)
     yo, yi = s[O].split(y, factor = ntx)
     s[O].bind(xii, thread_y())
     s[O].bind(yi, thread_x())
-    s[O].bind(xio, tvm.thread_axis('vthread', name='vth1'), no_unroll_vthread=True)
-    s[O].bind(yo, tvm.thread_axis('vthread', name='vth2'), no_unroll_vthread=True)
+    s[O].bind(xio, tvm.thread_axis('vthread', name='vth1'))
+    s[O].bind(yo, tvm.thread_axis('vthread', name='vth2'))
     s[Ol].compute_at(s[O], yi)
 
     b, x, h, y, k = s[Ol].leaf_iter_vars
     s[Ol].reorder(b, h, k, x, y)
     ko, ki = s[Ol].split(k, factor = red_tile)
-    # s[As].compute_at(s[Ol], ko)
-    # s[Vs].compute_at(s[Ol], ko)
-    # s[Al].compute_at(s[Ol], ki)
-    # s[Vl].compute_at(s[Ol], ki)
-    # s[Ol].peel(ko)
+    s[As].compute_at(s[Ol], ko)
+    s[Vs].compute_at(s[Ol], ko)
+    s[Al].compute_at(s[Ol], ki)
+    s[Vl].compute_at(s[Ol], ki)
+    s[Ol].peel(ko)
 
-    # _, x, h, y = s[As].leaf_iter_vars
-    # s[As].reorder(h, x, y)
-    # f = s[As].fuse(x, y)
-    # fo, fi = s[As].split(f, factor = ntx * nty * 4)
-    # fio, fii = s[As].split(fi, factor = ntx * 4)
-    # fiio, fiii = s[As].split(fii, factor = 4)
-    # s[As].bind(fio, thread_y())
-    # s[As].bind(fiio, thread_x())
-    # if not args.debug_functions: s[As].vectorize(fiii)
+    _, x, h, y = s[As].leaf_iter_vars
+    s[As].reorder(h, x, y)
+    f = s[As].fuse(x, y)
+    fo, fi = s[As].split(f, factor = ntx * nty * 2)
+    fio, fii = s[As].split(fi, factor = ntx * 2)
+    fiio, fiii = s[As].split(fii, factor = 2)
+    s[As].bind(fio, thread_y())
+    s[As].bind(fiio, thread_x())
+    if not args.debug_functions: s[As].vectorize(fiii)
 
-    # _, _, x, h, y = s[Vs].leaf_iter_vars
-    # s[Vs].reorder(h, x, y)
-    # f = s[Vs].fuse(x, y)
-    # fo, fi = s[Vs].split(f, factor = ntx * nty * 4)
-    # fio, fii = s[Vs].split(fi, factor = ntx * 4)
-    # fiio, fiii = s[Vs].split(fii, factor = 4)
-    # s[Vs].bind(fio, thread_y())
-    # s[Vs].bind(fiio, thread_x())
-    # if not args.debug_functions: s[Vs].vectorize(fiii)
+    _, _, x, h, y = s[Vs].leaf_iter_vars
+    s[Vs].reorder(h, x, y)
+    f = s[Vs].fuse(x, y)
+    fo, fi = s[Vs].split(f, factor = ntx * nty * 2)
+    fio, fii = s[Vs].split(fi, factor = ntx * 2)
+    fiio, fiii = s[Vs].split(fii, factor = 2)
+    s[Vs].bind(fio, thread_y())
+    s[Vs].bind(fiio, thread_x())
+    if not args.debug_functions: s[Vs].vectorize(fiii)
 
 G1, G2 = s.split_for_bin_packing([O], O, {O.op.axis[1]: lb_uf}, include_inputs=True)
 O1, O2 = G1[0], G2[0]
@@ -160,11 +160,11 @@ name = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0]
 out, batches = run_utils.lower_or_build(name, s, inputs, args, size_fn=size_fn, binds=binds,
                                         run_function=run_utils.get_bert_layer_run_fn(BATCH_SIZE))
 
-_, V, A, O  = out
-ctr = 0
-O = O.flatten()
-for length in batches[0]:
-    rounded64 = utils.ceilmult(length, 32)
-    this_extent = rounded64 * NUM_HEADS * HEAD_SIZE
-    print(length, np.mean(O[ctr:ctr + this_extent]))
-    ctr += this_extent
+# _, V, A, O  = out
+# ctr = 0
+# O = O.flatten()
+# for length in batches[0]:
+#     rounded64 = utils.ceilmult(length, 32)
+#     this_extent = rounded64 * NUM_HEADS * HEAD_SIZE
+#     print(length, np.mean(O[ctr:ctr + this_extent]))
+#     ctr += this_extent
