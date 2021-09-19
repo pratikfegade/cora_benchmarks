3a70135a92cd3d4cba70864e9408d08203e6eaf1
Pratik Fegade <pratikfegade@gmail.com>
2021-09-19 01:42:53 +0000

    WIP on vGEMM

--- run_utils.py
+++ run_utils.py
@@ -327,7 +327,7 @@ def get_bert_layer_run_fn(bs_var):
         return t_inputs, batches
     return bert_layer_run
 
-def get_vbatch_gemm_run_fn(bs_var):
+def get_vbatch_gemm_run_fn(bs_var, skip_m_k = False, no_scale=False):
     import tvm
     print('BS_VAR', bs_var)
     def run_vbatch_gemm(built, i_inputs_tensors, t_inputs_tensors, lw_args, args, pad_sum=None):
@@ -349,15 +349,23 @@ def get_vbatch_gemm_run_fn(bs_var):
             t_inputs = [batch_size] + [create_tvm_array(i, "float32", ctx, rmap=rmap, lw_args={}) for i in t_inputs_tensors[1:]]
             time = 0
             for i in range(len(ms)):
-                mb = np.ceil(ms[i] / args.tile_size).astype('int32')
-                nb = np.ceil(ns[i] / args.tile_size).astype('int32')
-                kb = np.ceil(ks[i] / args.tile_size).astype('int32')
+                if not no_scale:
+                    mb = np.ceil(ms[i] / args.tile_size).astype('int32')
+                    nb = np.ceil(ns[i] / args.tile_size).astype('int32')
+                    kb = np.ceil(ks[i] / args.tile_size).astype('int32')
+                else:
+                    mb = np.ceil(ms[i]).astype('int32')
+                    nb = np.ceil(ns[i]).astype('int32')
+                    kb = np.ceil(ks[i]).astype('int32')
 
-                l_inputs = [tvm.nd.array(mb, cpu_ctx), tvm.nd.array(nb, cpu_ctx), tvm.nd.array(kb, cpu_ctx)]
+                if skip_m_k:
+                    l_inputs = [tvm.nd.array(mb, cpu_ctx)]
+                else:
+                    l_inputs = [tvm.nd.array(mb, cpu_ctx), tvm.nd.array(nb, cpu_ctx), tvm.nd.array(kb, cpu_ctx)]
                 inputs = t_inputs + l_inputs + host_i_inputs + dev_i_inputs
                 time += execute(args.target, built, inputs, ctx, args.debug)
 
-            print("RESULTS", time / len(ms), sep=',')
+            print("RESULTS", batch_size, time / len(ms), sep=',')
             for i in range(len(t_inputs) - 1):
                 size_fn = {}
                 target = None
