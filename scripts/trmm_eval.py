22e288e860882269a8097c70b40be464eb184cff
Pratik Fegade <pratikfegade@gmail.com>
2022-01-28 14:13:25 +0000

    WIP on MKL trmm

--- scripts/trmm_eval.py
+++ scripts/trmm_eval.py
@@ -6,9 +6,20 @@ import argparse
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 CUBLAS_RUNNER = SCRIPT_DIR + '/../trmm/cublas/gemm_cublas'
-TVM_RUNNER = SCRIPT_DIR + '/../trmm/tvm/trmm.py'
+MKL_RUNNER = SCRIPT_DIR + '/../trmm/mkl/trmm'
+TVM_GPU_RUNNER = SCRIPT_DIR + '/../trmm/tvm/trmm.py'
+TVM_CPU_RUNNER = SCRIPT_DIR + '/../trmm/tvm/trmm_cpu.py'
 TACO_RUNNER = SCRIPT_DIR + '/../taco/taco_csr_trmm'
 PYTHON = 'python3'
+
+def get_mkl_runner(pad):
+    def run_mkl(m_size, n_size, err_file, args):
+        assert m_size == n_size
+        cmd = [MKL_RUNNER, str(m_size), '1' if pad else'0', str(100), str(1)]
+        out, err = run_cmd(cmd)
+        if err: print(err, file = err_file)
+        return com.extract_times(out, 1)[0]
+    return run_mkl
 
 def get_cublas_runner(pad):
     def run_cublas(m_size, n_size, err_file, args):
@@ -20,10 +31,11 @@ def get_cublas_runner(pad):
 
 def get_tvm_runner(balance, split):
     def run_tvm(m_size, n_size, err_file, args):
-        runner = TVM_RUNNER
+        runner = TVM_GPU_RUNNER if args.target == "cuda" else TVM_CPU_RUNNER
         cmd = [PYTHON, runner, '--target', com.get_tvm_target(target), '--m', str(m_size), '--n', str(n_size)]
         if balance: cmd += ['--load-balance']
         if split: cmd += ['--op-split']
+        print(' '.join(cmd))
         out, err = run_cmd(cmd)
         if err: print(err, file = err_file)
         return com.extract_times(out, 1)[0]
@@ -46,7 +58,7 @@ args = parser.parse_args()
 # ops = ['Sq', 'Th']
 ops = ['Sq']
 op_m_sizes = {
-    'Sq': [128, 256, 512, 1024, 2048, 4096, 8192],
+    'Sq': [512, 1024, 2048, 4096, 8192],
     # 'Th': [128, 256, 512, 1024, 2048, 4096, 8192, 8192*2, 8192*4],
 }
 
@@ -65,6 +77,15 @@ if args.target == 'cuda':
         # 'cora_balanced': get_tvm_runner(True, True),
         'taco': taco_runner,
     }
+else:
+    framework_funs = {
+        'mkl_nopad': get_mkl_runner(False),
+        'mkl_pad': get_mkl_runner(True),
+        'cora_unsplit': get_tvm_runner(False, False),
+        # 'cora_unbalanced': get_tvm_runner(False, True),
+        # 'cora_balanced': get_tvm_runner(True, True),
+    }
+
 
 results_out, results_err = get_out_files(args, 'trmm', 'a' if args.append else 'w')
 header = 'Op,Target,M,N'
--- trmm/tvm/trmm_cpu.py
+++ trmm/tvm/trmm_cpu.py
@@ -25,7 +25,7 @@ parser.add_argument('--dense-storage', dest='dense_storage', default=False, acti
 parser.add_argument('--gen-lib', dest='gen_lib', default=False, action='store_true')
 parser.add_argument('--only-prep-code', dest='only_prep_code', default=False, action='store_true')
 
-parser.add_argument('--m1', dest='m1', default=1, type=int)
+parser.add_argument('--m1', dest='m1', default=2, type=int)
 parser.add_argument('--m2', dest='m2', default=1, type=int)
 parser.add_argument('--n1', dest='n1', default=32, type=int)
 parser.add_argument('--n2', dest='n2', default=4, type=int)
@@ -51,6 +51,7 @@ A = te.ragged_placeholder((M, M), [md, kd], loop_ufs, name='A', width_ufs=None)
 
 B = te.placeholder((M, N), name='B')
 
+alpha = 2
 if args.op_split:
     def len_ufw(name, pad): return Ufw(name, "l", (pad, M), [md], [], lambda: lambda m: utils.floormult(m, pad))
     luf = len_ufw('s2k', 32).get_uf()
@@ -69,7 +70,7 @@ if args.op_split:
                             name = 'O2i', reduce_axis_ufs = [('k', Uf.from_constant('kd', 32, 'l'))], width_uf_lists=None)
 
     O2 = te.ragged_compute((M, N), [md, nd], loop_ufs,
-                           lambda ds: O1[ds[md], ds[nd]] + O2i[ds[md], ds[nd]],
+                           lambda ds: alpha*(O1[ds[md], ds[nd]] + O2i[ds[md], ds[nd]]),
                            name = 'O2', width_uf_lists=None)
 
     s = tvm.create_schedule([O1.op, O2.op])
@@ -78,43 +79,53 @@ else:
     luf = len_ufw('s2k', 256).get_uf()
 
     loop_ufs=[ls[0], ls[1]]
-    O = te.ragged_compute((M, N), [md, nd], loop_ufs,
-                          # lambda ds, rds: tvm.sum(tvm.tir.Cast('int32', rds['k'] < (ds[md] + 1)) *
-                                                  # A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
-                                                  # axis=rds['k'], dimensions = [kd]),
-                          lambda ds, rds: tvm.sum(A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
+    S = te.ragged_compute((M, N), [md, nd], loop_ufs,
+                          lambda ds, rds: tvm.sum(tvm.tir.Cast('int32', rds['k'] < (ds[md] + 1)) *
+                                                  A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
                                                   axis=rds['k'], dimensions = [kd]),
-                          name = 'O', reduce_axis_ufs = [('k', luf)], width_uf_lists=None)
+                          # lambda ds, rds: tvm.sum(A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
+                                                  # axis=rds['k'], dimensions = [kd]),
+                          name = 'S', reduce_axis_ufs = [('k', luf)], width_uf_lists=None)
+
+    O = te.ragged_compute((M, N), [md, nd], loop_ufs, lambda ds: alpha*S[ds[md], ds[nd]], name = 'O', width_uf_lists=None)
 
     s = tvm.create_schedule([O.op])
 
+def schedule_op(O, suffix, cache_write_tensor=None):
+    if cache_write_tensor is not None:
+        O_local = cache_write_tensor
+    else:
+        O_local, = s.cache_write([O], "local", storage_layout_mode='loop_layout')
 
-O_local, = s.cache_write([O], "local")
+    O_local_m_c, O_local_n_c, O_local_k = tuple(O_local.op.axis) + tuple(O_local.op.reduce_axis)
+    O_local_m_c_o_i, O_local_m_c_i = s[O_local].split(O_local_m_c, factor=4)
+    O_local_m_c_o_o_i, O_local_m_c_o_i = s[O_local].split(O_local_m_c_o_i, factor=64)
+    O_local_m_c_o_o_o, O_local_m_c_o_o_i = s[O_local].split(O_local_m_c_o_o_i, factor=1)
 
-O_local_m_c, O_local_n_c, O_local_k = tuple(O_local.op.axis) + tuple(O_local.op.reduce_axis)
-O_local_m_c_o_i, O_local_m_c_i = s[O_local].split(O_local_m_c, factor=4)
-O_local_m_c_o_o_i, O_local_m_c_o_i = s[O_local].split(O_local_m_c_o_i, factor=64)
-O_local_m_c_o_o_o, O_local_m_c_o_o_i = s[O_local].split(O_local_m_c_o_o_i, factor=1)
+    O_local_n_c_o_i, O_local_n_c_i = s[O_local].split(O_local_n_c, factor=64)
+    O_local_n_c_o_o_i, O_local_n_c_o_i = s[O_local].split(O_local_n_c_o_i, factor=1)
+    O_local_n_c_o_o_o, O_local_n_c_o_o_i = s[O_local].split(O_local_n_c_o_o_i, factor=1)
 
-O_local_n_c_o_i, O_local_n_c_i = s[O_local].split(O_local_n_c, factor=64)
-O_local_n_c_o_o_i, O_local_n_c_o_i = s[O_local].split(O_local_n_c_o_i, factor=1)
-O_local_n_c_o_o_o, O_local_n_c_o_o_i = s[O_local].split(O_local_n_c_o_o_i, factor=1)
+    O_local_k_o, O_local_k_i = s[O_local].split(O_local_k, factor=32)
+    s[O_local].reorder(O_local_m_c_o_o_o, O_local_n_c_o_o_o, O_local_m_c_o_o_i, O_local_n_c_o_o_i, O_local_k_o, O_local_m_c_o_i, O_local_n_c_o_i, O_local_k_i, O_local_m_c_i, O_local_n_c_i)
 
-O_local_k_o, O_local_k_i = s[O_local].split(O_local_k, factor=32)
-s[O_local].reorder(O_local_m_c_o_o_o, O_local_n_c_o_o_o, O_local_m_c_o_o_i, O_local_n_c_o_o_i, O_local_k_o, O_local_m_c_o_i, O_local_n_c_o_i, O_local_k_i, O_local_m_c_i, O_local_n_c_i)
+    O_m, O_n, = tuple(O.op.axis)
+    O_m_o, O_m_i = s[O].split(O_m, factor=256)
+    O_n_o, O_n_i = s[O].split(O_n, factor=64)
+    s[O].reorder(O_m_o, O_n_o, O_m_i, O_n_i)
 
-O_m, O_n, O_k = tuple(O.op.axis) + tuple(O.op.reduce_axis)
-O_m_o, O_m_i = s[O].split(O_m, factor=256)
-O_n_o, O_n_i = s[O].split(O_n, factor=64)
-s[O].reorder(O_m_o, O_n_o, O_m_i, O_n_i)
+    O_m_o_n_o_fused = s[O].fuse(O_m_o, O_n_o)
+    s[O].parallel(O_m_o_n_o_fused)
+    s[O_local].compute_at(s[O], O_m_o_n_o_fused)
+    s[O_local].pragma(O_local_m_c_o_o_o, "auto_unroll_max_step", 512)
+    s[O_local].pragma(O_local_m_c_o_o_o, "unroll_explicit", True)
+    s[O_local].vectorize(O_local_n_c_i)
 
-O_m_o_n_o_fused = s[O].fuse(O_m_o, O_n_o)
-s[O].parallel(O_m_o_n_o_fused)
-s[O_local].compute_at(s[O], O_m_o_n_o_fused)
-s[O_local].pragma(O_local_m_c_o_o_o, "auto_unroll_max_step", 512)
-s[O_local].pragma(O_local_m_c_o_o_o, "unroll_explicit", True)
-s[O_local].vectorize(O_local_n_c_i)
-
+if args.op_split:
+    schedule_op(O1, '1')
+    schedule_op(O2, '2', O2i)
+else:
+    schedule_op(O, '', S)
 
 if args.op_split: inputs = [[], [A, B, O1, O2]]
 else: inputs = [[], [A, B, O]]
