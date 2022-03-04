348303ee244a0f11e5efab3fdebf1007696e191d
Pratik Fegade <pratikfegade@gmail.com>
2022-03-03 21:02:06 -0500

    WIP on trmm

--- trmm/tvm/tmp/tmp_trmm_cpu.py
+++ trmm/tvm/tmp/tmp_trmm_cpu.py
@@ -6,15 +6,14 @@ from tvm import tir, te
 from tvm.te import RangeDimension as Dim
 from tvm.tir import UninterpFun as Uf, UfWrapper as Ufw
 import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../../")
 import utils
 import run_utils
 
 parser = run_utils.get_cmd_parser(no_options=True)
 parser.add_argument('--target', nargs='?', default='llvm')
 parser.add_argument('--dtype', dest='dtype', nargs='?', default='float32')
-parser.add_argument('--m', dest='m', default=1024, type=int)
-parser.add_argument('--n', dest='n', default=128, type=int)
+parser.add_argument('--m', dest='m', default=2048, type=int)
 parser.add_argument('--debug', dest='debug', default=False, action='store_true')
 parser.add_argument('--load-balance', dest='load_balance', default=False, action='store_true')
 parser.add_argument('--debug-code', dest='debug_code', default=None, type=str)
@@ -37,14 +36,13 @@ if args.m1 * args.m2 > 256: exit(0)
 if args.n1 * args.n2 > 64: exit(0)
 
 M = args.m
-N = args.n
 md = Dim('md')
 nd = Dim('nd')
 kd = Dim('kd')
 
 ls =  {
     0: Uf.from_constant('md', M, 'l'),
-    1: Uf.from_constant('nd', N, 'l'),
+    1: Uf.from_constant('nd', M, 'l'),
     2: Uf.from_constant('kd', M, 'l'),
 }
 
@@ -56,26 +54,26 @@ width_ufs=loop_ufs
 B = te.ragged_placeholder((M, M), [kd, nd], loop_ufs, name='B', width_ufs=None)
 
 # factor = args.m1 * args.m2
-factor = 256
+factor = 2048
 alpha = 2
 if args.op_split:
     def len_ufw(name, pad): return Ufw(name, "l", (pad, M), [md], [], lambda: lambda m: utils.floormult(m, pad))
     luf = len_ufw('s2k', factor).get_uf()
 
     loop_ufs=[ls[0], ls[1]]
-    O1 = te.ragged_compute((M, N), [md, nd], loop_ufs,
+    O1 = te.ragged_compute((M, M), [md, nd], loop_ufs,
                            lambda ds, rds: tvm.sum(A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
                                                    axis=rds['k'], dimensions = [kd]),
                            name = 'O1', reduce_axis_ufs = [('k', luf)], width_uf_lists=None)
 
-    O2i = te.ragged_compute((M, N), [md, nd], loop_ufs,
+    O2i = te.ragged_compute((M, M), [md, nd], loop_ufs,
                             lambda ds, rds: tvm.sum(tvm.tir.Cast('int32', utils.floormult(ds[md], 32) + rds['k'] < (ds[md] + 1)) *
                                                     A[ds[md], utils.floormult(ds[md], 32) + rds['k']] *
                                                     B[utils.floormult(ds[md], 32) + rds['k'], ds[nd]],
                                                     axis=rds['k'], dimensions = [kd]), name = 'O2i',
                             reduce_axis_ufs = [('k', Uf.from_constant('kd', 32, 'l'))], width_uf_lists=None)
 
-    O2 = te.ragged_compute((M, N), [md, nd], loop_ufs,
+    O2 = te.ragged_compute((M, M), [md, nd], loop_ufs,
                            lambda ds: alpha*(O1[ds[md], ds[nd]] + O2i[ds[md], ds[nd]]),
                            name = 'O2', width_uf_lists=None)
 
@@ -85,37 +83,90 @@ else:
     luf = len_ufw('s2k', factor).get_uf()
 
     loop_ufs=[ls[0], ls[1]]
-    S = te.ragged_compute((M, N), [md, nd], loop_ufs,
-                          lambda ds, rds: tvm.sum(tvm.tir.Cast('int32', rds['k'] < (ds[md] + 1)) *
-                                                  A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
-                                                  axis=rds['k'], dimensions = [kd]),
-                          # lambda ds, rds: tvm.sum(A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
+    S = te.ragged_compute((M, M), [md, nd], loop_ufs,
+                          # lambda ds, rds: tvm.sum(tvm.tir.Cast('int32', rds['k'] < (ds[md] + 1)) *
+                                                  # A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
                                                   # axis=rds['k'], dimensions = [kd]),
+                          lambda ds, rds: tvm.sum(A[ds[md], rds['k']] * B[rds['k'], ds[nd]],
+                                                  axis=rds['k'], dimensions = [kd]),
                           name = 'S', reduce_axis_ufs = [('k', luf)], width_uf_lists=None)
 
-    O = te.ragged_compute((M, N), [md, nd], loop_ufs, lambda ds: alpha*S[ds[md], ds[nd]],
+    O = te.ragged_compute((M, M), [md, nd], loop_ufs, lambda ds: alpha*S[ds[md], ds[nd]],
                           name = 'O', width_uf_lists=None)
 
     s = tvm.create_schedule([O.op])
 
+tile = 256
+def intrin_gemv(m, n, r):
+    a = te.placeholder((m,r), name="a")
+    b = te.placeholder((r,n), name="b")
+
+    k = te.reduce_axis((0,r), name="k")
+    c = te.ragged_compute((m, n), [md, nd], [Uf.from_constant('m', m, 'l'), Uf.from_constant('n', n, 'l')],
+                          lambda ds: tvm.sum(a[ds[md], k] * b[k, ds[nd]], axis=k, dimensions=[kd]),
+                          name = 'O', width_uf_lists=None)
+
+    Ab = tvm.tir.decl_buffer(a.shape, a.dtype, name="A", offset_factor=1, strides=[M, 1])
+    Bb = tvm.tir.decl_buffer(b.shape, b.dtype, name="B", offset_factor=1, strides=[M, 1])
+    Cb = tvm.tir.decl_buffer(c.shape, c.dtype, name="C", offset_factor=1, strides=[tile, 1], scope='local')
+
+    def intrin_func(ins, outs):
+        aa, bb = ins
+        cc = outs[0]
+
+        def _body():
+            ib = tvm.tir.ir_builder.create()
+            ib.emit(tvm.tir.call_packed("tvm.contrib.cblas.matmul_no_thread",
+                                        aa,
+                                        bb,
+                                        cc,
+                                        False,
+                                        False,
+                                        1.0,
+                                        1.0))
+            return ib.get()
+
+        def _reduce_reset():
+            ib = tvm.tir.ir_builder.create()
+            ib.emit(tvm.tir.call_extern("int32", "gemv_reset", cc.access_ptr("w"), m, n))
+            return ib.get()
+
+        def _reduce_update():
+            return _body()
+
+        return _body(), _reduce_reset(), _reduce_update()
+
+    return te.decl_tensor_intrin(c.op, intrin_func, binds={a: Ab, b: Bb, c: Cb})
+
+
+def gemv_impl():
+    cc_code = """
+      extern "C" int gemv_reset(float *cc, int m, int n) {
+        for (int i = 0; i < m; ++i) {
+          for (int j = 0; j < n; ++j) {
+            cc[i * n + j] = 0.0;
+          }
+        }
+        return 0;
+      }
+    """
+    from tvm.contrib import util, clang
+
+    temp = util.tempdir()
+    ll_path = temp.relpath("temp.ll")
+    ll_code = clang.create_llvm(cc_code, output=ll_path)
+    return ll_code
+
+
+gemv = intrin_gemv(tile, tile, tile)
+
 def schedule_op(O, suffix, cache_write_tensor=None):
     residual = (cache_write_tensor is not None) and args.op_split
     if cache_write_tensor is not None:
-        O_l = cache_write_tensor
+        Ol = cache_write_tensor
     else:
-        O_l, = s.cache_write([O], "local", storage_layout_mode='loop_layout')
+        Ol, = s.cache_write([O], "local", storage_layout_mode='loop_layout')
 
-    O_l_m_c, O_l_n_c, O_l_k = tuple(O_l.op.axis) + tuple(O_l.op.reduce_axis)
-
-    O_l_m_c_o_i, O_l_m_c_i = s[O_l].split(O_l_m_c, factor=4)
-    O_l_m_c_o_o_i, O_l_m_c_o_i = s[O_l].split(O_l_m_c_o_i, factor=64)
-
-    O_l_n_c_o_i, O_l_n_c_i = s[O_l].split(O_l_n_c, factor=64)
-    O_l_n_c_o_o_i, O_l_n_c_o_i = s[O_l].split(O_l_n_c_o_i, factor=4)
-    O_l_n_c_o_o_o, O_l_n_c_o_o_i = s[O_l].split(O_l_n_c_o_o_i, factor=8)
-
-    O_l_k_o, O_l_k_i = s[O_l].split(O_l_k, factor=64)
-    s[O_l].reorder(O_l_m_c_o_o_i, O_l_n_c_o_o_o, O_l_n_c_o_o_i, O_l_k_o, O_l_m_c_o_i, O_l_n_c_o_i, O_l_k_i, O_l_m_c_i, O_l_n_c_i)
 
     O_m, O_n = tuple(O.op.axis)
     O_m_o_i, O_m_i = s[O].split(O_m, factor=256)
@@ -123,23 +174,21 @@ def schedule_op(O, suffix, cache_write_tensor=None):
     O_n_o_i, O_n_i = s[O].split(O_n, factor=256)
     O_n_o_o, O_n_o_i = s[O].split(O_n_o_i, factor=8)
     s[O].reorder(O_m_o_i, O_n_o_o, O_n_o_i, O_m_i, O_n_i)
-    # s[O_l].compute_at(s[O], O_n_o_i)
-
-    # O_m_o_o_fused_n_o_o_fused = s[O].fuse(O_m_o_o, O_n_o_o)
-    # s[O].parallel(O_m_o_o_fused_n_o_o_fused)
+    s[Ol].compute_at(s[O], O_n_o_i)
 
 
-    # s[O_l].pragma(O_l_m_c_o_o_i,a "auto_unroll_max_step", 0)
-    # s[O_l].pragma(O_l_m_c_o_o_i, "unroll_explicit", True)
-    # s[O_l].unroll(O_l_k_i)
-    # s[O_l].unroll(O_l_m_c_i)
-    # s[O_l].vectorize(O_l_n_c_i)
-
-    # s[O].mark_no_bounds_check()
-    # s[O_l].mark_no_bounds_check()
+    m, n, k = s[Ol].leaf_iter_vars
+    ko, ki = s[Ol].split(s[Ol].op.reduce_axis[0], factor = 256)
+    s[Ol].reorder(ko, m, n, ki)
+    # s[Ol].pragma(ko, "import_llvm", gemv_impl())
+    s[Ol].tensorize(m, gemv)
 
 
-    if cache_write_tensor is None: return [O.op, O_l.op]
+    s[Ol].set_scope("local")
+    O_m_o_o_fused_n_o_o_fused = s[O].fuse(O_m_o_i, O_n_o_o)
+    s[O].parallel(O_m_o_o_fused_n_o_o_fused)
+
+    if cache_write_tensor is None: return [O.op, Ol.op]
     else: return []
 
 substitute_ops = []
